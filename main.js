be819a1f0e1264dde7cba97ce5f10164cc0effba
Daniela Olarte <danielaolarte123@gmail.com>
2022-03-26 17:29:27 -0500

    Funcionalidad de la operación 'Agregar Issue'

---
+++ main.js
@@ -0,0 +1,39 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+
+  var issueId = chance.guid();
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+
+  
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+  }
+  
+  if (localStorage.getItem('issues') === null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+  
+  document.getElementById('issueInputForm').reset();
+ 
+  fetchIssues();
+  
+  e.preventDefault(); 
+}
+
+function fetchIssues () {
+
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+}
