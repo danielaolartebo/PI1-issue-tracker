eb0ff8e83d7e409f030c390f78a10ae2e45ccad9
ALEXJR2002 <alexssjr2002@gmail.com>
2022-03-28 14:04:39 -0500

    deleteIssue function implemented

--- main.js
+++ main.js
@@ -76,3 +76,16 @@ function setStatusClosed (id) {
     fetchIssues();
   }
 
+  function deleteIssue (id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    
+    for(var i = 0; i < issues.length; i++) {
+      if (issues[i].id == id) {
+        issues.splice(i, 1);
+      }
+    }
+    
+    localStorage.setItem('issues', JSON.stringify(issues));
+    
+    fetchIssues();
+  }
